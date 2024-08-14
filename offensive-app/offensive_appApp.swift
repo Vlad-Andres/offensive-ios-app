5dd73b98aff1ac821b98c6330d135bd7ec88576e
Vlad-Andres <bruno993409@gmail.com>
2024-08-14 19:58:50 +0200

    Add initial app

---
+++ offensive-app/offensive_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  offensive_appApp.swift
+//  offensive-app
+//
+//  Created by Vlad Andres on 14/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct offensive_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
