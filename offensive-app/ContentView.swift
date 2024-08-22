2491864f4b73560e9dc4f1022112258ce42ff346
Vlad-Andres <bruno993409@gmail.com>
2024-08-22 20:03:53 +0200

    Added some backgorund functionality

--- offensive-app/ContentView.swift
+++ offensive-app/ContentView.swift
@@ -8,15 +8,64 @@
 import SwiftUI
 
 struct ContentView: View {
+    @ObservedObject var dataFetcher = DataFetcher()
+
     var body: some View {
+
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text(dataFetcher.data)
+                .onAppear {
+                    dataFetcher.fetchData()
+                }
+                .padding()
         }
         .padding()
     }
+    
+//    func startBackgroundTask() {
+//        Timer.scheduledTimer(withTimeInterval: 6.0, repeats: true) { _ in
+//            fetchData()
+//        }
+//    }
+    
+    class DataFetcher: ObservableObject {
+        @Published var data: String = "Fetching..."
+        private var url: URL = URL(string: "https://evilinsult.com/generate_insult.php?lang=en&type=json")!
+        
+        var timer: Timer?
+
+        init() {
+            startTimer()
+        }
+
+        func startTimer() {
+            timer = Timer.scheduledTimer(withTimeInterval: 2.0, repeats: true) { _ in
+                self.fetchData()
+            }
+        }
+
+        func fetchData() {
+            var test: String = String(Int.random(in: 1..<9))
+            data = test
+            print(test)
+//
+            let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+                print("Sending..." . data)
+                if let data = data, let responseString = String(data: data, encoding: .utf8) {
+                    print("Got it")
+                    DispatchQueue.main.async {
+                        self.data = responseString
+                    }
+                }
+            }
+//    
+//            task.resume()
+        }
+    }
+
 }
 
 #Preview {
